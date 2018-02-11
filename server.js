6313683020a45f4ce49968daa10a1bba10c8b808
Glitch (giant-bucket) <none>
2018-02-11 04:28:54 +0000

    ⏳🐗 Checkpoint server.js:293729/352

--- server.js
+++ server.js
@@ -1,37 +1,48 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
+var axios = require('axios');
+var bodyParser = require('body-parser');
+var FeedParser = require('feedparser');
+
 var app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+var req = axios.get('http://website.stmikbumigora.ac.id/index.php/category/pengumuman/feed/')
+  .then(function (response) {
+    //console.log(response);
+    var feedparser = new FeedParser();
+    
+    response.data.pipe(feedparser);
+
+    feedparser.on('readable', function () {
+      // This is where the action is!
+      var stream = this; // `this` is `feedparser`, which is a stream
+      var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+      var item;
+
+      while (item = stream.read()) {
+        console.log(item);
+      }
+    });
+  });
+
+
+app.post("/", function(request, response) {
+  
+});
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
 
 // Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+// var dreams = [
+//   "Find and count some sheep",
+//   "Climb a really tall mountain",
+//   "Wash the dishes"
+// ];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
